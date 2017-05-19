ee40b6f7a0b4bc921370a0ca907cb6845955572e
CAI Yudong <yudong.cai@soprasteria.com>
2017-05-19 11:10:08 +0100

    improve comments

--- src/main/java/com/ycai/foursquare/api/model/Venue.java
+++ src/main/java/com/ycai/foursquare/api/model/Venue.java
@@ -2,7 +2,7 @@ package com.ycai.foursquare.api.model;
 
 /**
  * TODO
- * Venue - this api's model to adapts the official FourSquare Venue and used by this api   
+ * Venue - this api's model to adapt the official FourSquare Venue, and to be used by this api   
  */
 public class Venue {
 
