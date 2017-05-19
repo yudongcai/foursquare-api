df44834c297a3a484de78609a992db5a2d09b687
CAI Yudong <yudong.cai@soprasteria.com>
2017-05-19 09:05:54 +0100

    add VenueControllerIT

---
+++ src/test/java/com/ycai/foursquare/api/controller/VenueControllerIT.java
@@ -0,0 +1,31 @@
+package com.ycai.foursquare.api.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class VenueControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testSearch() throws Exception {
+
+        this.mockMvc.perform(get("/venue/search")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.response.venues").isNotEmpty());
+    }
+
+}
